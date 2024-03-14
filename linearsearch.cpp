94895951e200cd85ece8ca55ad8a267bf3f02c79
Suyash Labde <suyashlabde123@gmail.com>
2024-03-15 00:04:14 +0530

    linear search done

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -1,27 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int linearSearch(int arr[],int n,int num){
-    for(int i = 0 ; i<n;i++){
-        if(num == arr[i]);
-        return i;
+int linearSearch(int arr[], int n, int num)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (num == arr[i])
+        {
+            cout << "Element is present at : ";
+            return i; // Return index of the element found
+        }
     }
-    return -1;}
+    cout << "Element is not present";
+    return -1; // Return -1 if element not found
+}
 
-
-
-int main(){
-    cout<<"Enter the Size"<<endl;
+int main()
+{
+    cout << "Enter the Size" << endl;
     int size;
-    cin>>size;
+    cin >> size;
     int arr[100];
-    for(int i = 0;i<size;i++){
-        cin>>arr[i];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
     }
-    cout<<"Enter the element you want to search";
+    cout << "Enter the element you want to search";
     int num;
-    cin>>num;
+    cin >> num;
+    
+    int result = linearSearch(arr, size, num);
+    cout << result; // Output the index of the element found (-1 if not found)
     return 0;
-    int result = linearSearch(arr,size,num);
-    cout<<result;
 }
