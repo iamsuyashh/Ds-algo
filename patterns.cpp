712d48b18ea76ca645074d1421cfc88a93635533
Suyash Labde <suyashlabde123@gmail.com>
2024-03-23 20:41:27 +0530

    All patterns

---
+++ patterns.cpp
@@ -0,0 +1,46 @@
+// list of patterns using below:
+// 1.Simple Pyramid Pattern
+// 2.Flipped Simple Pyramid Pattern
+
+#include <iostream>
+using namespace std;
+void simplePyramid()
+{
+    int n;
+    cout << "\n Enter the size of the pyramid : ";
+    cin >> n;
+    int i = 0;
+    while (i <= n)
+    {
+        int j = 0;
+        while (j < i)
+        {
+            cout << "*";
+            j++;
+        }
+        cout << " " << endl;
+        i++;
+    }
+};
+
+int main()
+{
+    int number;
+    cout << endl;
+    cout << " ************  Select the number of patterns from the list below : ************";
+    cout << "\n 1. Click to get the simple pyramid.\n"
+         << endl;
+    cout << "Enter the number of your choice : ";
+    cin >> number;
+
+    switch (number)
+    {
+    case 1:
+        simplePyramid();
+        break;
+
+    default:
+        cout << "Please Enter a valid Number to see the patterns";
+        break;
+    }
+}
