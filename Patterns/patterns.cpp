c5dd264f9ea6544c5ee553bf3cc3a29173ebddcd
Suyash Labde <suyashlabde123@gmail.com>
2024-03-23 20:49:59 +0530

    restructured repository

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -4,6 +4,25 @@
 
 #include <iostream>
 using namespace std;
+void  FlippedsimplePyramid(){
+     int n;
+    cout << "\n Enter the size of the pyramid : ";
+    cin >> n;
+    int i = 0;
+    while (i <= n)
+    {
+        int j = 0;
+        while (j < i)
+        {
+            cout << "*";
+            j++;
+        }
+        cout << " " << endl;
+        i++;
+    }
+}
+
+
 void simplePyramid()
 {
     int n;
@@ -28,8 +47,11 @@ int main()
     int number;
     cout << endl;
     cout << " ************  Select the number of patterns from the list below : ************";
-    cout << "\n 1. Click to get the simple pyramid.\n"
-         << endl;
+    cout << "\n 1. Click to get the simple pyramid.\n"<< endl;
+    cout << "\n 2. Click to get the flipped simple pyramid.\n"<< endl;
+    
+    // Taking the input from the user
+    
     cout << "Enter the number of your choice : ";
     cin >> number;
 
@@ -37,6 +59,9 @@ int main()
     {
     case 1:
         simplePyramid();
+        break;
+    case 2:
+        FlippedsimplePyramid();
         break;
 
     default:
