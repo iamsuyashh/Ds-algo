e1c2d22251e74a2153d6cf0e896ff68ad363e42a
Suyash Labde <suyashlabde123@gmail.com>
2024-04-19 09:13:25 +0530

    reversed array coding ninjas done using vector

--- Leetcode and coding ninjas/reverseArray.cpp
+++ Leetcode and coding ninjas/reverseArray.cpp
@@ -5,8 +5,8 @@
 
 // Example:
 
-// We have an array ARR = {1, 2, 3, 4, 5, 6} and M = 3 , considering 0 
-// based indexing so the subarray {5, 6} will be reversed and our 
+// We have an array ARR = {1, 2, 3, 4, 5, 6} and M = 3 , considering 0
+// based indexing so the subarray {5, 6} will be reversed and our
 // output array will be {1, 2, 3, 4, 6, 5}.
 // Detailed explanation ( Input/output format, Notes, Images )
 // Constraints:
@@ -26,20 +26,21 @@
 #include <vector>
 using namespace std;
 
-vector<int> reverse(vector<int> v)
+vector <int> reversedArray(vector<int> v, int m)
 {
-    int s = 0;
-    int e = v.size() - 1;
-    while (s <= e)
+    int s = m + 1;
+    int e = v.size()-1;
+    while (s<e)
     {
-        swap(v[s], v[e]);
+        swap(v[s],v[e]);
         s++;
         e--;
     }
     return v;
+    
 }
 
-void print(vector<int> v)
+void printArray(vector<int> v)
 {
     for (int i = 0; i < v.size(); i++)
     {
@@ -51,17 +52,21 @@ void print(vector<int> v)
 int main()
 {
     int size;
-    int input;
-    cout << "Enter the size of the vector: ";
+    cout << "Enter the Size of the Array" << " ";
     cin >> size;
+    int m;
+    cout << "Enter the position you want to Reverse Array from" <<endl;
+    cin >> m;
+    int input;
     vector<int> v;
     for (int i = 0; i < size; i++)
     {
+        cout << "Enter the elements in the array"<<endl;
         cin >> input;
         v.push_back(input);
     }
 
-    vector<int> ans = reverse(v);
-    print(ans);
+    vector<int> ans = reversedArray(v,m);
+    printArray(ans);
     return 0;
 }
