3191aa8c39e97a5f174f2d1270fb1e335bdd28a6
Suyash Labde <suyashlabde123@gmail.com>
2024-04-20 20:34:53 +0530

    leet code ques Moves Zeros

--- Leetcode and coding ninjas/mergeTwoArrays.cpp
+++ Leetcode and coding ninjas/mergeTwoArrays.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void merge(int arr1[], int n, int arr2[], int m, int arr3[]) {
 
     int i = 0, j = 0;
-    int k = 0;
+    int k = 0;  
     while( i<n && j<m) {
         if(arr1[i] < arr2[j]){
             arr3[k++] = arr1[i++];
@@ -22,7 +22,7 @@ void merge(int arr1[], int n, int arr2[], int m, int arr3[]) {
 
     //copy kardo second array k remaining element ko
     while(j<m) {
-        arr2[k++] = arr2[j++];
+        arr3[k++] = arr2[j++];
     }
 }
 
