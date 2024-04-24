5d69a1002c1f51adb2da65390ebfc88825b1708f
Suyash Labde <suyashlabde123@gmail.com>
2024-04-24 19:58:59 +0530

    another leetcode ques

---
+++ Leetcode and coding ninjas/maxOccurence.cpp
@@ -0,0 +1,19 @@
+// GEEKS FOR GEEKS QUESITON
+
+// Given a string str of lowercase alphabets. The task is to find the maximum occurring character in the string str. If more than one character occurs the maximum number of time then print the lexicographically smaller character.
+// Example 1:
+
+// Input:
+// str = testsample
+// Output: e
+// Explanation: e is the character which
+// is having the highest frequency.
+// Example 2:
+
+// Input:
+// str = output
+// Output: t
+// Explanation:  t and u are the characters
+// with the same frequency, but t is lexicographically smaller.
+
+
