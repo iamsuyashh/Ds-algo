6e0ab7f5c36783b11c29a91ddc7ee585bee93c78
Suyash Labde <suyashlabde123@gmail.com>
2024-06-21 21:59:59 +0530

    reversed array using recursion

---
+++ Recursion/Reverse.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void reverseArray(int arr[], int start, int end) {
+    if (start >= end) {
+        return;
+    }
+
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+    reverseArray(arr, start + 1, end - 1);
+}
+
+void reverseArrayRecursive(int arr[], int size) {
+    reverseArray(arr, 0, size - 1);
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    reverseArrayRecursive(arr, size);
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
