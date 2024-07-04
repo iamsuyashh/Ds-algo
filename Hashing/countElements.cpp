66a4296f0e4177595ba7473358c61b33540963e6
Suyash Labde <suyashlabde123@gmail.com>
2024-07-04 18:33:11 +0530

    Counting elements using hashing

--- Hashing/countElements.cpp
+++ Hashing/countElements.cpp
@@ -9,6 +9,21 @@ int main(){
     {
         cin>>arr[i];
     }
+
+    // logic for hash
+    int hash[13] = {0};
+    for(int i = 0; i<n;i++){
+        hash[arr[i]]+=1;    
+    }
     
+
+    int q;
+    cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+        cout<<"The number of times "<<number<<" appears in the array is :"<<hash[number]<<endl;
+    
+    }
     return 0;
 }
