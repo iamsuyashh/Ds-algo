4a39d3f1b2826f7d4bd9cd173ff1304e358ba901
Suyash Labde <suyashlabde123@gmail.com>
2024-07-18 22:29:53 +0530

    Selection sort modified

--- Hashing/countCharacters.cpp
+++ Hashing/countCharacters.cpp
@@ -1,3 +1,6 @@
+// this program will count the number of times each character appears in a string using hash maps
+
+// IN ordered hash map it willl give output in an sequence manner but in under ordered hash map it will give output in random manner
 #include <iostream>
 using namespace std;
 
@@ -9,7 +12,7 @@ int main()
     int hash[256] = {0};
     for (int i = 0; i < s.size(); i++)
     {
-
+        
         hash[s[i]] += 1;
     }
 
--- Hashing/hashmap.cpp
+++ Hashing/hashmap.cpp
@@ -1,27 +1,31 @@
-#include<iostream>
+#include <iostream>
 #include <map>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 
     // compute the hash
-    map<int,int> mpp;
-    for(int i = 0 ; i<n;i++ ){
+    map<int, int> mpp;
+    for (int i = 0; i < n; i++)
+    {
         mpp[arr[i]]++;
     }
 
     int q;
-    cin>>q;
-    while(q--){
+    cin >> q;
+    while (q--)
+    {
         int number;
-        cin>>number;
-        cout<<"The number of times "<<number<<" appears in the array is :"<<mpp[number]<<endl;
+        cin >> number;
+        cout << "The number of times " << number << " appears in the array is :" << mpp[number] << endl;
     }
     return 0;
 }
