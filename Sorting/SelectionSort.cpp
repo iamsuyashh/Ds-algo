12bb8b6e2052da8ea33358400c966567a74ca5c7
Suyash Labde <suyashlabde123@gmail.com>
2024-07-20 20:16:46 +0530

    Updated selection sort code

--- Sorting/SelectionSort.cpp
+++ Sorting/SelectionSort.cpp
@@ -1,40 +1,31 @@
-// Little modified Selecetion sort
-
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n)
-{
-    for (int i = 0; i <= n-2; i++)
-    {
-        int minIndex = i;
-        for (int j = i; j <= n-1; j++)
-        {
-            if (arr[j] < arr[minIndex]){
-                minIndex = j;
+
+void Sort(int arr[],int n){
+        for(int i  = 0 ; i<n-1;i++){
+            for(int j = i+1 ; j<n;j++){
+                int minIndex = i;
+                if(arr[j]<arr[minIndex]){
+                    swap(arr[j],arr[minIndex]);
+                }
             }
-            
         }
-        // swapping logic
-        int temp = arr[minIndex];
-        arr[minIndex] = arr[i];
-        arr[i] = temp;
-    }
 }
 
-int main()
-{
+int main(){
     int n;
-    cout << "Enter the size of the array" << endl;
-    cin >> n;
-    int arr[n];
-     for (int i = 0; i < n; i++)
+    cout<<"Enter the size of the array";
+    cin>>n;
+    int arr[10];
+    for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        cin>>arr[i];
     }
-    selectionSort(arr, n);
-   for(int i = 0 ; i<n ; i++){
-    cout<<arr[i]<<" ";
-   }
+   Sort(arr,n);
+    for(int i = 0;i<n;i++){
+            cout<<""<<arr[i]<<" ";
+    }
+    
     return 0;
 }
