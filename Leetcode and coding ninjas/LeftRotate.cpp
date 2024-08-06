52971e24b5e8dd81ae825e267c3ec70e59faf6f1
Suyash Labde <suyashlabde123@gmail.com>
2024-08-06 08:39:33 +0530

    leftrotate of coding ninjas done

---
+++ Leetcode and coding ninjas/LeftRotate.cpp
@@ -0,0 +1,34 @@
+// Problem statement
+// Given an array 'arr' containing 'n' elements, rotate this array left once and return it.
+// Rotating the array left by one means shifting all elements by one place to the left and moving the first element to the last position in the array.
+// Example:
+// Input: 'a' = 5, 'arr' = [1, 2, 3, 4, 5]
+// Output: [2, 3, 4, 5, 1]
+
+
+#include<iostream>
+
+using namespace std;
+
+int rotate(int arr[],int n){
+    int temp = arr[0];
+    for(int i = 1 ; i<n ;  i++){
+        arr[i-1] = arr[i];
+    }
+    arr[n-1] = temp;
+     for (int i = 0; i < n; i++)
+    {
+        cout << "" << arr[i] << " ";
+    }
+}
+
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    int n = 5;
+    cout<<"Array after left rotation : ";
+    rotate(arr, n);
+   
+
+
+    return 0;
+}
---
+++ Sorting/MergeSort.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void mergeSort(vector<int> &arr,int low , int high )
+
+int main(){
+    
+    return 0;
+}
