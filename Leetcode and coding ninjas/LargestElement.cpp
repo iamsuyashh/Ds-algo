5cf488979b46a9aca95107ddb7dd255e9a87ba9b
Suyash Labde <suyashlabde123@gmail.com>
2024-08-20 20:11:34 +0530

    updated

--- Leetcode and coding ninjas/LargestElement.cpp
+++ Leetcode and coding ninjas/LargestElement.cpp
@@ -6,31 +6,22 @@
 // Output: 5
 // Explanation: From the array {1, 2, 3, 4, 5}, the largest element is 5.
 
-
 #include<iostream>
 using namespace std;
 
-int findLargestElement(int arr[], int n){
-    int max = arr[0];
-    for(int i  = 1; i < n; i++){
-        if(arr[i] > max){
-            max = arr[i];
+int largest(int arr[],int n){
+    int largest = arr[0];
+    for(int i = 0 ; i<n;i++){
+        if(arr[i] > largest){
+            largest = arr[i];
         }
-
     }
-    return max;
-    }
-
-int main(){
-    int arr1[] = {2,5,1,3,0};
-  int n = 5;
-  int max = findLargestElement(arr1, n);
-  cout << "The largest element in the array is: " << max << endl;
- 
-  int arr2[] =  {8,10,5,7,9};
-  n = 5;
-  max = findLargestElement(arr2, n);
-  cout << "The largest element in the array is: " << max<<endl;
-  return 0;
+    return largest;
 }
 
+int main(){
+    int arr[5] = {1, 2, 3, 4, 5};
+    int n = 5;
+    cout<<largest(arr,n);
+    return 0;
+}
